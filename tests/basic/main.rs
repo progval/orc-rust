datafusion-contrib/datafusion-orc

commit e3b0be4735894155c01ab4173a045b8ca72abb3b

Introduce PrimitiveValueDecoder::decode_spaced for nullable streams

src/array_decoder/mod.rs
@@ -17,7 +17,7 @@
 
 use std::sync::Arc;
 
-use arrow::array::{ArrayRef, BooleanArray, BooleanBuilder, PrimitiveArray, PrimitiveBuilder};
+use arrow::array::{ArrayRef, BooleanArray, BooleanBuilder, PrimitiveArray};
 use arrow::buffer::NullBuffer;
 use arrow::datatypes::ArrowNativeTypeOp;
 use arrow::datatypes::{ArrowPrimitiveType, Decimal128Type};
@@ -75,28 +75,14 @@ impl<T: ArrowPrimitiveType> PrimitiveArrayDecoder<T> {
         parent_present: Option<&[bool]>,
     ) -> Result<PrimitiveArray<T>> {
         let present = derive_present_vec(&mut self.present, parent_present, batch_size);
-
+        let mut data = vec![T::Native::ZERO; batch_size];
         match present {
             Some(present) => {
-                let count = present.iter().filter(|&&p| p).count();
-                let mut data = vec![T::Native::ZERO; count];
-                self.iter.decode(data.as_mut_slice())?;
-
-                let mut data = data.iter();
-                let mut builder = PrimitiveBuilder::<T>::with_capacity(batch_size);
-                for is_present in present {
-                    if is_present {
-                        // Safe unwrap as we guarantee length above
-                        builder.append_value(*data.next().unwrap());
-                    } else {
-                        builder.append_null();
-                    }
-                }
-                let array = builder.finish();
+                self.iter.decode_spaced(data.as_mut_slice(), &present)?;
+                let array = PrimitiveArray::<T>::new(data.into(), Some(present.into()));
                 Ok(array)
             }
             None => {
-                let mut data = vec![T::Native::ZERO; batch_size];
                 self.iter.decode(data.as_mut_slice())?;
                 let array = PrimitiveArray::<T>::from_iter_values(data);
                 Ok(array)

src/encoding/mod.rs
@@ -102,6 +102,31 @@ pub trait PrimitiveValueDecoder<V>: Iterator<Item = Result<V>> {
             Ok(())
         }
     }
+
+    /// Decode into out according to the true elements in present, which must be
+    /// the same length as out.
+    fn decode_spaced(&mut self, out: &mut [V], present: &[bool]) -> Result<()> {
+        debug_assert_eq!(out.len(), present.len());
+        for (out_n, _) in out
+            .iter_mut()
+            .zip(present.iter())
+            .filter(|(_, &is_present)| is_present)
+        {
+            match self.next() {
+                Some(r) => {
+                    *out_n = r?;
+                }
+                None => {
+                    // TODO: more descriptive error
+                    return OutOfSpecSnafu {
+                        msg: "Array length less than expected",
+                    }
+                    .fail();
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 pub fn get_unsigned_rle_reader<R: Read + Send + 'static>(

src/array_decoder/string.rs
@@ -122,16 +122,8 @@ impl<T: ByteArrayType> GenericByteArrayDecoder<T> {
         } else {
             batch_size
         };
-        let lengths = self
-            .lengths
-            .by_ref()
-            .take(elements_to_fetch)
-            .collect::<Result<Vec<_>>>()?;
-        debug_assert_eq!(
-            lengths.len(),
-            elements_to_fetch,
-            "less lengths than expected in ByteArray"
-        );
+        let mut lengths = vec![0; elements_to_fetch];
+        self.lengths.decode(&mut lengths)?;
         let total_length: i64 = lengths.iter().sum();
         // Fetch all data bytes at once
         let mut bytes = Vec::with_capacity(total_length as usize);

src/encoding/float.rs
@@ -58,6 +58,7 @@ impl Float for f64 {
     }
 }
 
+// TODO: rename to FloatDecoder
 pub struct FloatIter<T: Float, R: std::io::Read> {
     reader: R,
     phantom: std::marker::PhantomData<T>,
@@ -78,6 +79,20 @@ impl<T: Float, R: std::io::Read> PrimitiveValueDecoder<T> for FloatIter<T, R> {
         self.reader.read_exact(&mut buf).context(IoSnafu)?;
         for (out_float, bytes) in out.iter_mut().zip(buf.chunks(T::BYTE_SIZE)) {
             *out_float = T::from_le_bytes(bytes);
+        }
+        Ok(())
+    }
+
+    fn decode_spaced(&mut self, out: &mut [T], present: &[bool]) -> Result<()> {
+        debug_assert_eq!(out.len(), present.len());
+        let mut buf = vec![0; T::BYTE_SIZE];
+        for (out_float, _) in out
+            .iter_mut()
+            .zip(present.iter())
+            .filter(|(_, &is_present)| is_present)
+        {
+            self.reader.read_exact(&mut buf).context(IoSnafu)?;
+            *out_float = T::from_le_bytes(&buf);
         }
         Ok(())
     }

src/error.rs
@@ -26,6 +26,7 @@ use snafu::Location;
 use crate::proto;
 use crate::schema::DataType;
 
+// TODO: consolidate error types? better to have a smaller set?
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub))]
 pub enum OrcError {

progval/orc-rust

commit 46f488ee005918a20696479d55a9d611fc385324

Avoid unnecessary null buffer when decoding a primitive array with no null
This undoes a change in behavior caused by
https://github.com/datafusion-contrib/datafusion-orc/commit/e3b0be4735894155c01ab4173a045b8ca72abb3b

src/array_decoder/mod.rs
@@ -98,12 +98,12 @@ impl<T: ArrowPrimitiveType> PrimitiveArrayDecoder<T> {
             derive_present_vec(&mut self.present, parent_present, batch_size).transpose()?;
         let mut data = vec![T::Native::ZERO; batch_size];
         match present {
-            Some(present) => {
+            Some(present) if present.null_count() > 0 => {
                 self.iter.decode_spaced(data.as_mut_slice(), &present)?;
                 let array = PrimitiveArray::<T>::new(data.into(), Some(present));
                 Ok(array)
             }
-            None => {
+            _ => {
                 self.iter.decode(data.as_mut_slice())?;
                 let array = PrimitiveArray::<T>::from_iter_values(data);
                 Ok(array)

tests/basic/data/no_nulls.py
@@ -1,0 +1,15 @@
+"""
+Generates a file with a 'present' stream for each column, but no actual nulls in it
+"""
+
+from pathlib import Path
+
+import pyorc
+
+path = Path(__file__).parent / "no_nulls.orc"
+
+with path.open("wb") as data:
+    #with pyorc.Writer(data, "struct<col0:int,col1:string>") as writer:
+    with pyorc.Writer(data, pyorc.Struct(col0=pyorc.Int(), col1=pyorc.String())) as writer:
+        writer.write((1, "row 1"))
+        writer.write((2, "row 2"))

tests/basic/main.rs
@@ -625,3 +625,54 @@ pub fn assert_batches_eq(batches: &[RecordBatch], expected_lines: &[&str]) {
         expected_lines, actual_lines
     );
 }
+
+/// Tests a file with a 'present' stream for each column, but no actual nulls in it
+#[test]
+pub fn no_nulls_test() {
+    let path = basic_path("no_nulls.orc");
+    let reader = new_arrow_reader_root(&path);
+    let schema = reader.schema();
+    let batches = reader.collect::<Result<Vec<_>, _>>().unwrap();
+
+    let expected = [
+        "+------+-------+",
+        "| col0 | col1  |",
+        "+------+-------+",
+        "| 1    | row 1 |",
+        "| 2    | row 2 |",
+        "+------+-------+",
+    ];
+    assert_batches_eq(&batches, &expected);
+
+    let expected_file_schema = Arc::new(Schema::new(vec![
+        Field::new(
+            "col0",
+            DataType::Int32,
+            true, // this shouldn't change unless no_nulls.orc was incorrectly regenerated
+        ),
+        Field::new(
+            "col1",
+            DataType::Utf8,
+            true, // this shouldn't change unless no_nulls.orc was incorrectly regenerated
+        ),
+    ]));
+    assert_eq!(schema, expected_file_schema);
+
+    let expected_batch_schema = Arc::new(Schema::new(vec![
+        Field::new(
+            "col0",
+            DataType::Int32,
+            false,
+        ),
+        Field::new(
+            "col1",
+            DataType::Utf8,
+            false,
+        ),
+    ]));
+    for batch in &batches {
+        assert_eq!(batch.schema(), expected_batch_schema);
+        assert!(batch.column_by_name("col0").unwrap().nulls().is_none());
+        assert!(batch.column_by_name("col1").unwrap().nulls().is_none());
+    }
+}